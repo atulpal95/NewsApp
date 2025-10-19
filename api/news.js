00c2b4c3d2c242cba7680010d3eb1e50d9d46728
Atul Pal <palatul9559@gmail.com>
2025-10-20 01:06:35 +0530

    Update API key in news.js

--- api/news.js
+++ api/news.js
@@ -3,7 +3,7 @@
 export default async function handler(req, res) {
   const { q = "general", lang = "en", page = 1 } = req.query;
 
-  const apiKey = "b445839247464771bee592750c03ed56";
+  const apiKey = "5a836692700a00e9939802d1a231bcd5";
   const url = `https://gnews.io/api/v4/search?q=${q}&lang=${lang}&max=24&page=${page}&apikey=${apiKey}`;
 
   try {
@@ -19,4 +19,5 @@ export default async function handler(req, res) {
     console.error("Error fetching from GNews:", error);
     res.status(500).json({ error: "Failed to fetch from GNews" });
   }
+
 }
